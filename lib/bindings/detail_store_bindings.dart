1736275b302fc1815b28c6957ef3c7f78f687bf4
kotadakku <eus3.g5@gmail.com>
2022-04-02 17:08:16 +0700

    edit_detail

--- lib/bindings/detail_store_bindings.dart
+++ lib/bindings/detail_store_bindings.dart
@@ -1,12 +1,12 @@
-import 'package:app_casynet/controller/detail_app_controller.dart';
 import 'package:get/get.dart';
 import 'package:get/get_instance/src/bindings_interface.dart';
 
 import '../controller/detail_store_controller.dart';
 
+
 class DetailStoreBindings extends Bindings{
   @override
   void dependencies() {
-    Get.lazyPut(() => DetailAppController());
+    Get.lazyPut(() => DetailStoreController());
   }
 }
